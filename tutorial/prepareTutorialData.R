cec4a53eeb165d864242e6d546548702cf7c623e
Lyuba Bozhilova <bozhilov@dunlin.stats.ox.ac.uk>
2019-10-04 18:28:20 +0100

    tutorial with yeast data -- start

--- tutorial/prepareTutorialData.R
+++ tutorial/prepareTutorialData.R
@@ -3,29 +3,40 @@
 ################################
 
 # First make sure you have installed and unzipped the data. You can find it here:
-# https://horvath.genetics.ucla.edu/html/CoexpressionNetwork/Rpackages/WGCNA/Tutorials/index.html
+# https://www.ebi.ac.uk/gxa/experiments/E-MTAB-5174/Downloads
 
 # Parse the data
 setwd("tutorial/")
-liverData <- as.data.frame(read.table("LiverFemale3600.csv", sep=",", header=TRUE))
+yeastData <- read.table("E-MTAB-5174-query-results.tsv", sep="\t", header=TRUE)
 
 # Remove unnecessary metadata columns
-liverData <- liverData[,-c(1, 3:8)]
+yeastData <- yeastData[,-1]
 
-# Rename the "gene_symbol" column to "Name"
-colnames(liverData)[1] <- "Name"
+# Remove p-values
+yeastData <- yeastData[,-seq(3, 53, 2)]
 
-# Remove gene symbols which occur on more than one row
-badGenes <- names(table(liverData$Name)[table(liverData$Name)>1])
-liverData <- subset(liverData, !(Name %in% badGenes))
+# Rename the "Gene.ID" column to "Name"
+colnames(yeastData)[1] <- "Name"
+
+# Remove gene symbols which occur on more than one row (none in this case)
+badGenes <- names(table(yeastData$Name)[table(yeastData$Name)>1])
+yeastData <- subset(yeastData, !(Name %in% badGenes))
+
+# Remove genes with more than 25% of data points missing
+fracAllowed <- 0.25
+badGenes <- which(rowSums(is.na(yeastData[,-1]))>fracAllowed*(ncol(yeastData)-1)) # 1183 genes
+yeastData <- yeastData[-badGenes,] # 1920 left
 
 # Reduce to the 200 most variable genes, as measured by their median absolute deviation
-geneMADs <- apply(liverData[,-1], 1, mad)
-names(geneMADs) <- liverData$Name
+geneMADs <- apply(yeastData[,-1], 1, mad, na.rm=TRUE)
+names(geneMADs) <- yeastData$Name
 geneMADs <- sort(geneMADs, decreasing=TRUE)
 goodGenes <- names(geneMADs[1:200])
-liverData <- subset(liverData, Name %in% goodGenes)
+yeastData <- subset(yeastData, Name %in% goodGenes)
+
+# Remove sample names
+colnames(yeastData)[-1] <- paste0("sample", 1:(ncol(yeastData)-1))
 
 # At this stage we won't do any data normalisation or apply additional filters
 # Save results
-save(liverData, file="tutorialData.RData")
+save(yeastData, file="tutorialData.RData")
